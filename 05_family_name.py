2aa285d838550b094c59e3b42ad245156addbcc6
sumit-dev-ai <127115568+sumit-dev-ai@users.noreply.github.com>
2023-03-06 20:57:59 +0530

    Delete 05_family_name.py

--- 05_family_name.py
+++
@@ -1,8 +0,0 @@
-a= input("my name is:")
-b= input("my father name is:")
-c= input("my mother name is:")
-d= input("my brother name is:")
-print("My name is :",a)
-print("My father name is",b)
-print("my mother name is:",c)
-print("my brother name is:",d)
